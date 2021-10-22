1624916a3ac7c5d0b79320e6fdcbdb0eea91ab64
Adam <40191591+Mauvlans@users.noreply.github.com>
2021-10-22 16:17:19 -0700

    Add files via upload

---
+++ Remove-AA-WMIBridge.ps1
@@ -0,0 +1,12 @@
+﻿function DisableKiosk {
+
+$nameSpaceName="root\cimv2\mdm\dmmap"
+$className="MDM_AssignedAccess"
+$obj = Get-CimInstance -Namespace $namespaceName -ClassName $className
+$obj.Configuration = $NULL
+
+    
+}
+
+    DisableKiosk
+ 
